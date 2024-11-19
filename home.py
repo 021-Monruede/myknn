f44eca677c37971aa1abcd98798649a41ca1a5b7
021-Monruede <156268832+021-Monruede@users.noreply.github.com>
2024-11-19 15:02:49 +0700

    Update home.py

--- home.py
+++ home.py
@@ -2,13 +2,12 @@ import streamlit as st
 import pandas as pd
 
 
-st.ttie("Website Developing using Python")
+st.title("Website Developing using Python")
 st.header("Website Developing using Python")
 
 st.image('./img/monruede.jpg')
 st.subheader("Monruede Chomchuen")
 
-df=pd.read_csv('./data/itis-3.csv')
+dt=pd.read_csv('./data/itis-3.csv')
 st.header("ข้อมูลดอกไม้")
 st.write(dt.head(10))
-
